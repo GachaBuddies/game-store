9e5c2ad8a0db980e9320204e7c2b4e26dcbe05ba
GachaBuddies <thanhngoc3009@gmail.com>
2024-10-31 09:16:12 +0700

    Nguyen Thanh Ngoc

---
+++ models/db.php
@@ -0,0 +1,13 @@
+<?php
+class Data {
+    public static $connection;
+
+    public function __construct() {
+        if (!self::$connection) {
+            self::$connection = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+            self::$connection->set_charset(DB_CHARSET);
+        }
+        return self:$connection;
+    }
+}
+?>
