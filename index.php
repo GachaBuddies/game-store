06febd25a83c84b6e36c3cf9b03898bd27424bcc
GachaBuddies <thanhngoc3009@gmail.com>
2024-11-14 07:17:25 +0700

    Nguyen Thanh Ngoc

--- index.php
+++ index.php
@@ -34,7 +34,7 @@ $showMore = $totalGenres > $initialGenreCount;
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Game Store</title>
-    <link rel="stylesheet" href="css/styles.css?">
+    <link rel="stylesheet" href="css/styles.css">
 </head>
 <body>
 
